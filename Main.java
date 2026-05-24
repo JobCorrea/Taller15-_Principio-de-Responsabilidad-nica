4ae88c0171d38bd10e7f665063222248420dd594
Job Daniel Correa <jobcorre0912@gmail.com>
2026-05-24 11:52:21 -0500

    Pruba objeto Libro

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(String[] args) {
-        
+        Libro libro = new Libro("El Quijote", "Planeta", 1605);
+
+        ReporteLibroService reporte = new ReporteLibroService();
+        reporte.generarReporte(libro);
+
+        LibroRepository repository = new LibroRepository();
+        repository.guardarLibro(libro);
     }
 }
